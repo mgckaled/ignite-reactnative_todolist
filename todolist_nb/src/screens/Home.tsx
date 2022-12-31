f4410d3b1ef9c572160e8a7779bc36e23529e411
mgckaled <marcelgckaled@gmail.com>
2022-12-31 10:30:43 -0300

    [nativebase]feat: create functions and include comppnent properties to main screen.

--- todolist_nb/src/screens/Home.tsx
+++ todolist_nb/src/screens/Home.tsx
@@ -1,4 +1,6 @@
-import { Divider, HStack, Stack, VStack } from 'native-base'
+import { Divider, FlatList, HStack, Stack, VStack } from 'native-base'
+import { useState } from 'react'
+import { Alert, Keyboard } from 'react-native'
 
 import { Button } from '@components/Button'
 import { Counter } from '@components/Counter'
@@ -7,22 +9,92 @@ import { Header } from '@components/Header'
 import { Input } from '@components/Input'
 import { TaskCard } from '@components/TaskCard'
 
+export type TaskProps = {
+	id: string
+	title: string
+	checked: boolean
+}
+
 export function Home() {
+	const [tasks, setTask] = useState<TaskProps[]>([])
+	const [text, setText] = useState('')
+
+	function handleAddItem() {
+		if (!text) {
+			return Alert.alert('Atenção', 'Informe o nome da tarefa.')
+		}
+
+		const newItem = {
+			id: new Date().getTime().toString(),
+			title: text,
+			checked: false
+		}
+
+		setTask(prev => [...prev, { ...newItem }])
+		setText('')
+		Keyboard.dismiss()
+	}
+
+	function handleCheck(id: string) {
+		const newTasks = tasks.map(item => {
+			if (item.id === id) item.checked = !item.checked
+			return item
+		})
+
+		setTask(newTasks)
+	}
+
+	function handleDelete(id: string) {
+		Alert.alert('Atenção', 'Tem certeza que deseja remover essa tarefa?', [
+			{
+				text: 'Não',
+				style: 'cancel'
+			},
+			{
+				text: 'Sim',
+				style: 'destructive',
+				onPress: () => {
+					const newTasks = tasks.filter(item => item.id !== id)
+					setTask(newTasks)
+				}
+			}
+		])
+	}
+
 	return (
 		<Stack flex={1} bg="gray.500">
 			<Header />
-			<VStack mx={6}>
+			<VStack flex={1} mx={6}>
 				<HStack justifyContent="center" mt={-9}>
-					<Input mr={2} />
-					<Button />
+					<Input mr={2} value={text} setValue={setText} />
+					<Button handleAddItem={handleAddItem} />
 				</HStack>
-				<HStack my={4} justifyContent="space-between">
-					<Counter label="Criadas" value={0} color="blue.500" />
-					<Counter label="Concluídas" value={0} color="purple.500" />
+				<HStack my={5} justifyContent="space-between">
+					<Counter label="Criadas" value={tasks.length} color="blue.500" />
+					<Counter
+						label="Concluídas"
+						value={tasks.filter(item => item.checked).length}
+						color="purple.500"
+					/>
 				</HStack>
 				<Divider mb={6} bg="gray.300" />
-				<TaskCard />
-				<EmpytItems />
+				<FlatList
+					data={tasks}
+					keyExtractor={item => item.id}
+					renderItem={({ item }) => (
+						<TaskCard
+							data={item}
+							key={item.id}
+							handleCheck={handleCheck}
+							handleDelete={handleDelete}
+						/>
+					)}
+					ListEmptyComponent={() => <EmpytItems />}
+					showsVerticalScrollIndicator={false}
+					_contentContainerStyle={{
+						paddingBottom: 16
+					}}
+				/>
 			</VStack>
 		</Stack>
 	)
